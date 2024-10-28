b4648c4c5a9d6a6362558d84360a37e1c71bc5d6
Anderson Mancini <anderson@neotix.com.br>
2024-10-28 00:20:13 -0300

    Minor updates to colors

--- src/store.js
+++ src/store.js
@@ -5,7 +5,7 @@ const state = proxy({
     '#FFD700',  // Giallo Orion (Gold Yellow)
     '#CC0000',  // Rosso Mars (Bright Red)
     '#DA291C',  // Rosso Bia (Deep Red)
-    '#FEFEFE',  // Bianco Isis (Pure White)
+    '#d0d0d0',  // Bianco Isis (Pure White)
     '#0A0A0A',  // Nero Noctis (Gloss Black)
     '#007F0E',  // Verde Mantis (Bright Green)
     '#FF6F00',  // Arancio Borealis (Vibrant Orange)
